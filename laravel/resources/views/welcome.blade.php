dd1dcd7f6019b3f3ed027b2217944d5a74516738
Nalis1000 <nico@blatters.net>
2022-03-15 09:25:28 +0100

    Added: -LoginForm -RegisterForm -Navbar -Rusticus-Logo

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,3 +1,14 @@
 <x-layout>
-
+    </head>
+    <div class="container content"></div>
+        <div class="container search">
+            <form method="post" action="#">
+                <div class="input-group">
+                    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
+                           aria-describedby="search-addon"/>
+                    <input type="submit" class="btn btn-outline-primary" value="Search">
+                </div>
+            </form>
+        </div>
+    </div>
 </x-layout>
