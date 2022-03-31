4ce653e6c5b9c547a2b10e737c32babf714b213e
vmadmin <vmadmin@webvm-wan.localdomain>
2022-03-31 16:19:16 +0200

    blade for cart and reservation (later not yet working correctly)

---
+++ laravel/app/Http/Controllers/ReservationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reservation;
+use Illuminate\Http\Request;
+
+class ReservationController extends Controller
+{
+    public function showReservation()
+    {
+        $userId = 1; // TODO: get userid
+        if (Reservation::where('user_id', '=', $userId)->first()) {
+            $reservationProducts = Reservation::where('user_id', '=', $userId)->first()->reservation_has_product()->get();
+            return view('reservation.reservation', ['reservationProducts' => $reservationProducts]);
+        }
+        return view('reservation.reservation');
+    }
+
+    public function checkout()
+    {
+    }
+}
