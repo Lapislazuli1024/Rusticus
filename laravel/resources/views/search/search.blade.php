adc52b67fca3c8e32926cd6104a0804d05542f59
vmadmin <vmadmin@webvm-wan.localdomain>
2022-05-17 09:43:51 +0200

    search and product fix

--- laravel/resources/views/search/search.blade.php
+++ laravel/resources/views/search/search.blade.php
@@ -1,7 +1,5 @@
 <x-layout>
     <div class="row">
-
-
         <div class="col-9">
             <div class="card">
                 <div class="card-header">
@@ -9,29 +7,30 @@
                 </div>
                 <div class="card-body" id="class">
                     @foreach($result as $product)
-                        <div class="card">
+                    @if($product->stock_quantity >= 0)
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="{{asset($product->image)}}" class="img-layout">
                             <div class="card-header">
-                                <h5>{{$product->name}}</h5>
+                                <h5 class="card-title">{{$product->name}}</h5>
+                                @can('IsProductOwner', $product)
+                                <a href="/product/edit/{{$product->id}}" class="btn btn-success float-end">Bearbeiten</a>
+                                @endcan
                             </div>
-                            <div class="card-body">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="list-group-item">
-                                                Menge: {{$product->stock_quantity}}
-                                            </div>
-                                            <div class="list-group-item">
-                                                Preis pro kg: {{$product->price}}
-                                            </div>
-                                        </div>
-                                        <div class="col-1 vr"></div>
-                                        <div class="col">
-                                            <img class="img-thumbnail" src="{{asset('pictures/User.png')}}">
-                                        </div>
+                            <div class="card-body container-flex">
+                                <div class="list-group list-group-flush">
+                                    <p> Menge: {{$product->stock_quantity}}</p>
+                                    <p> Preis pro {{$product->unit_of_measure->description}}: {{$product->price}}</p>
+                                </div>
+                                <div>
+                                    <div class="card text-center">
+                                        <a href="/product/show/{{$product->id}}" class="btn btn-success float-end">Mehr</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    @endif
                     @endforeach
                 </div>
             </div>
