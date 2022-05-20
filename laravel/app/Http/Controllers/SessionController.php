6937b39b24a602e2b604154073a6f9ddc59633e2
Lapislazuli1024 <Reiner_Zufall@outlook.com>
2022-05-20 17:01:47 +0200

    Show Reservation Link

--- laravel/app/Http/Controllers/SessionController.php
+++ laravel/app/Http/Controllers/SessionController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Customer;
 use App\Models\Sub_category;
 use App\Models\Unit_of_measure;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
